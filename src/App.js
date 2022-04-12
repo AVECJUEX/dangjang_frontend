33003b07b9561a37898919d839b82c929ce1ad1a
lxxragos <lxxragos@naver.com>
2022-04-12 17:48:39 +0900

    [정진] 회원가입 페이지 구현

--- src/App.js
+++ src/App.js
@@ -13,11 +13,16 @@ import BoardUpdate from "./component/board/board_update";
 import FreeBoardList from "./component/freeboard/freeboard_list";
 import FreeBoardWrite from "./component/freeboard/freeboard_write";
 import FreeBoardView from "./component/freeboard/freeboard_view";
+
 import QnaList from "./component/qna/qna_list"
 import QnaView from'./component/qna/qna_view';
 import QnaWrite from'./component/qna/qna_write';
 import QnaCommentWrite from "./component/qna/qnacomment_write";
 import QnaUpdate from "./component/qna/qna_update"
+
+
+import MyList from "./component/mypage/my_list"
+
 import RegisterPage from "./component/member/RegisterPage"
 
 import LoginModal from "./component/Modal/LoginModal";
@@ -94,21 +99,20 @@ function App() {
 
               <Route exact path="qna/*" element={<QnaList/>}/>
               <Route exact path="qna/write" element={<QnaWrite/>}/>
-              
               <Route path="/qnaUpdate/:qna_seq" element={<QnaUpdate/>}/>
               <Route path="/qnacommentwrite/:qna_seq"  element={<QnaCommentWrite/>} />
  
               
-              
-            
-
-
+              {/*회원가입  */}
               <Route path="register" element={<RegisterPage />} />
               <Route path="loginmodal" element={<LoginModal />} />
               <Route path="loginpage" element={<LoginPage />} />
-
               <Route path="findIdmodal" element={<FindIfModal />} />
               
+              {/* 마이페이지 */}
+              <Route exact path="mypage/*" element={<MyList/>}/>
+
+
               <Route path="test" element={<PZoomImage />} />
               
             </Routes>
---
+++ src/component/mypage/my_menu.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Input from "../Input";
+import Label from "../Label";
+import { Routes, Route, Outlet, Link, NavLink } from "react-router-dom";
+
+
+
+
+
+
+function MyMenu() {
+ 
+  // const onMouse=(e)=>{
+  
+    
+  //   e.target.style.background = "#e5e8eb";
+  //   //document.getElementById('qnali').style.backgroundColor="#e5e8eb";
+  // }
+  // const mouseDown=(e)=>{
+  //   e.target.style.background = "#ffffff";
+  //   //document.getElementById('qnali').style.backgroundColor="#ffffff";
+  // }
+  
+  return(
+    <div style={{display : 'inline-block', width : '15%', padding : '0px'}}>
+
+    <NavLink className={({ isActive }) => 'mymenu'+ (isActive ? '-active' : '')} to="">내 정보 수정</NavLink>
+    <NavLink className={({ isActive }) => 'mymenu'+ (isActive ? '-active' : '')} to="" >내가 쓴 글</NavLink>
+    <NavLink className={({ isActive }) => 'mymenu'+ (isActive ? '-active' : '')} to="" >내 판매</NavLink>
+    <NavLink className={({ isActive }) => 'mymenu'+ (isActive ? '-active' : '')} to="">채팅</NavLink>
+
+    
+  
+ 
+    </div>
+  )
+}
+export default MyMenu;
